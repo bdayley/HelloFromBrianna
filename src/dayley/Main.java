159a3ffbfcf27242294c8e6d00ef8eb5c82431e1
bdayley <col04002@byui.edu>
2021-05-22 20:45:29 -0600

    changed name

--- src/dayley/Main.java
+++ src/dayley/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, my name is Brianna");
+        System.out.println("Hello, my name is Bri");
         System.out.println("Hello Brianna, my name is Aaron");
     }
 }
