da60ede61da62511285c2fd6ea12941d934dec45
bdayley <col04002@byui.edu>
2021-05-24 21:46:38 -0600

    stretch challenge

--- src/dayley/Main.java
+++ src/dayley/Main.java
@@ -6,5 +6,8 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Bri");
         System.out.println("Hello Brianna, my name is Aaron");
+
+        System.out.println("try the stretch challenge");
+        System.out.println("trying the stretch challenge");
     }
 }
