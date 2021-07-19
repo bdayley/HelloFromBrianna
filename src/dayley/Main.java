5d655e4d2df1050e72c0a081963f4ba06b43f2ef
chris2thebell <chris2thebell@gmail.com>
2021-07-18 22:29:37 -0600

    Chris Added

--- src/dayley/Main.java
+++ src/dayley/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Bri");
         System.out.println("Hello Brianna, my name is Aaron");
+        System.out.println("Hello Brianna, my name is Chris");
 
         System.out.println("try the stretch challenge");
         System.out.println("trying the stretch challenge");
